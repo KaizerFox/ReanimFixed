94644f39ec27215a31084c792658561c05f6f495
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2021-01-12 12:39:43 -0500

    Update ReanimFixed.lua

--- ReanimFixed.lua
+++ ReanimFixed.lua
@@ -1,6 +1,8 @@
 loadstring(game:HttpGet("https://paste.ee/r/uk77k/0"))()
+wait()
 
 if _G.Rename == true then
 local char = game.Workspace:FindFirstChild(""..game.Players.LocalPlayer.Name)
 char.Name = "CloneCharacter"
 end
+wait()
